852aa103b93cf0c1d74b31d095fb93c13ee020b2
SHARUNJOSEPH <josephsharun9895@gmail.com>
2021-08-04 14:36:20 +0530

    new

--- src/app/post.service.ts
+++ src/app/post.service.ts
@@ -13,30 +13,30 @@ export class PostService {
       new Date(),
       10
     ),
-    new Post(
-      'teck',
-      'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
-      'https://itchronicles.com/wp-content/uploads/2021/01/technology-impact-on-life-1024x566.jpg',
-      'test@test.com',
-      new Date(),
-      15
-    ),
-    new Post(
-      'humbi',
-      'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
-      'https://media-cdn.tripadvisor.com/media/photo-s/14/cf/f3/ed/thousand-year-old-temple.jpg',
-      'test@test.com',
-      new Date(),
-      11
-    ),
-    new Post(
-      'Nature',
-      'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
-      'https://images.indianexpress.com/2021/02/Green-solution.jpg',
-      'test@test.com',
-      new Date(),
-      15
-    ),
+    // new Post(
+    //   'teck',
+    //   'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
+    //   'https://itchronicles.com/wp-content/uploads/2021/01/technology-impact-on-life-1024x566.jpg',
+    //   'test@test.com',
+    //   new Date(),
+    //   15
+    // ),
+    // new Post(
+    //   'humbi',
+    //   'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
+    //   'https://media-cdn.tripadvisor.com/media/photo-s/14/cf/f3/ed/thousand-year-old-temple.jpg',
+    //   'test@test.com',
+    //   new Date(),
+    //   11
+    // ),
+    // new Post(
+    //   'Nature',
+    //   'bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl bl',
+    //   'https://images.indianexpress.com/2021/02/Green-solution.jpg',
+    //   'test@test.com',
+    //   new Date(),
+    //   15
+    // ),
   ];
 
   //current post -1
